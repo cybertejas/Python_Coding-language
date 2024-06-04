8985ad66ce1346395b071bdaef008a8136e590ce
Arvinder Pal <arvinderpal10@gmail.com>
2024-06-04 21:19:25 +0530

    Variables and Data types in Python

---
+++ Online_Class/Day3/dataTypes.py
@@ -0,0 +1,64 @@
+#Data type - 
+
+#Grade Card 
+
+"""
+Marks - 68.7 , 60 
+Grades - A, B, C, D, E, F
+Name - Tejas - F
+
+"""
+
+x =20 
+x= "Hello"
+print(x)
+
+#type()
+
+print(type(35))
+print(type("Hello"))
+print(type(True))
+print(type(34.5))
+
+
+#Why do we need to know the Data Type :
+#print(x + 234) 
+
+
+#Casting - Converting one data type into other
+
+"""
+Float to int 
+"""
+# a = 34.5
+# b = a 
+# c = int(a) 
+# print(a,b,c)
+
+"""
+Int to Float
+"""
+w = 30
+t = float(30)
+print(w,t)
+
+
+"""
+Integer to String
+"""
+u = str(123)
+print(u)
+
+# v = int("Hello")
+# print(v)
+
+f = str(34.5)
+print(type(f))
+
+
+
+
+
+
+
+
---
+++ Online_Class/Day3/variables.py
@@ -0,0 +1,56 @@
+#NameError: name 'x' is not defined
+#print(x)
+
+# print(x)
+# x =10
+
+x =10
+x ="Hello"
+
+# print(x)
+# print(x+10)
+# print(x+ " World")
+
+#Multiple Variable declaration
+x,y = 23, 30
+x, y,z = 20 , "Hello", True
+
+"""
+Case 1
+"""
+#a,b,c = 0
+#print(a,b,c)
+
+
+"""
+Case 2
+"""
+# a,b,c=23,40
+# print(a,b,c)
+
+"""
+Case 3
+"""
+# a,b,c=23,40,36
+# print(a,b,c)
+
+"""
+Case 4
+"""
+# a,b,c=23,"", 
+# print(a,b,c)
+
+"""
+Case 5
+"""
+# a,b,c=2e10,40,36
+# print(a,b,c)
+
+"""
+Case 6
+"""
+# a,b,c=23,40,36,345
+# print(a,b,c)
+
+
+
