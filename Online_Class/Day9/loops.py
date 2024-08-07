9a6e10feda180e7ce3a73fecb57fb8f14b54bbf9
Arvinder Pal <arvinderpal10@gmail.com>
2024-08-07 21:56:20 +0530

    Loops in Python

---
+++ Online_Class/Day9/loops.py
@@ -0,0 +1,68 @@
+# Loops -> For repeating task 
+
+# print("Hello Google")
+# print("Hello Microsoft")
+# print("Hello Google")
+# print("Hello Google")
+# print("Hello Google")
+# print("Hello Google")
+
+# We can't do it for large numbers. 
+# Customization is not possible 
+
+# Initialisation - Starting point - will be executed only once
+# Condition Check - It sets a condition for looping 
+# Increement - i + 1 
+
+# i=0  - by default  
+# Range - Number of times loop will execute = range , It will stop once reaches to range value 
+# for i in range(6):
+#     print(i)
+
+# We can give the starting point
+# for i in range(3,10):
+#     print(i)
+
+#        range(start , last , step)
+# for i in range(3,10,2):
+#     print(i)
+
+# for i in range(10,0,-1):
+#     print(i)
+
+# for i in range(-10,1,1):
+#     print(i)
+
+# Continue 
+# for i in range(10):
+#     if i == 4:
+#         continue
+#     else:
+#         print(i)
+
+# or - |
+# and - &
+# not - !
+
+# for i in range(10):
+#     if i == 4 or i == 5 or i==6:
+#         continue
+#     else:
+#         print(i)
+
+# Break
+# for i in range(10):
+#     if i==4:
+#         break
+#     else:
+#         print(i)
+
+# for x in "TejasPandey":
+#     if x =="e":
+#         continue
+#     print(x)
+
+
+
+
+
