c304707d62f7587a139580d323aafbc0db559377
Arvinder Pal <arvinderpal10@gmail.com>
2024-08-08 21:57:09 +0530

    Star Patterns

---
+++ Online_Class/Day9/StarPatterns/01.Patterns.py
@@ -0,0 +1,48 @@
+# Patterns
+
+# for i in range(5):
+#     for j in range(5):
+#         print("*",end=" ")
+    # print()
+for i in range(0,5):
+    for j in range(0,i+1):
+        print("*", end=" ")
+    print()
+
+print(" --------- ")
+
+for i in range(6,0,-1):
+    for j in range(0,i-1):
+        print("*", end=" ")
+    print()
+
+print(" --------------------- ")
+
+# for i in range(0,5):
+#     for j in range(0,5):
+#         print("*",end=" ")
+#     print()
+
+
+# for i in range(0,5):
+#     for j in range(0,i+1):
+#         print("1", end=" ")
+#     print()
+
+
+# for i in range(0,5):
+#     for j in range(0,i+1):
+#         print(i, end=" ")
+#     print()
+
+
+# for i in range(0,5):
+#     for j in range(0,i+1):
+#         print(j, end=" ")
+#     print()
+# num=1
+# for i in range(0,5):
+#     for j in range(0,i+1):
+#         print(num, end=" ")
+#         num = num+1
+#     print()
