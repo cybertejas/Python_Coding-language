d80554c7a1caf5dca2a82f69e73144f965f6b6c5
Arvinder Pal <arvinderpal10@gmail.com>
2024-08-25 10:02:29 +0530

    Python functions

---
+++ Online_Class/Day10/004.Function.py
@@ -0,0 +1,22 @@
+# def Greet(user):
+#     return "Hello " +  user
+
+# print(Greet("Arvinder"))
+# # name = Greet("Tejas")
+# print(Greet("Tejas") + " Pandey")
+
+
+def add(a,b):
+    return a+b
+def sub(a,b):
+    return a-b
+
+# def sub2(a,b):
+#     return a-b
+
+# print("The addition of 20 and 10 is :",add(20,10)," and difference is :",+ sub(20,10))
+
+# print("print() is a function in Python - to print values to console.")
+
+
+
