c2fea709275272f031adbe124289f4893cacbc97
cybertejas <tejascyberethics@gmail.com>
2024-08-25 10:15:20 +0530

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	new file:   Tasks/Projects/Project_1/Calculator.py
    	renamed:    Tasks/Task_6/README -> Tasks/Projects/README
    	renamed:    Tasks/Task_7/README -> Tasks/Tasks/README
    	renamed:    Tasks/Task_1/README -> Tasks/Tasks/Task_1/README
    	renamed:    Tasks/Task_1/main.py -> Tasks/Tasks/Task_1/main.py
    	renamed:    Tasks/Task_2/README -> Tasks/Tasks/Task_2/README
    	renamed:    Tasks/Task_2/main.py -> Tasks/Tasks/Task_2/main.py
    	renamed:    Tasks/Task_3/README -> Tasks/Tasks/Task_3/README
    	renamed:    Tasks/Task_3/main.py -> Tasks/Tasks/Task_3/main.py
    	renamed:    Tasks/Task_4/main.py -> Tasks/Tasks/Task_4/main.py
    	renamed:    Tasks/Task_5/groceryList.py -> Tasks/Tasks/Task_5/groceryList.py
    	renamed:    Tasks/Task_6/Compiled_1/main.py -> Tasks/Tasks/Task_6/Compiled_1/main.py
    	renamed:    Tasks/Task_6/Compiled_2/main.py -> Tasks/Tasks/Task_6/Compiled_2/main.py
    	renamed:    Tasks/Task_6/Complied_3/main.py -> Tasks/Tasks/Task_6/Complied_3/main.py
    	renamed:    Online_Class/Day10/Projects/Calculator.py -> Tasks/Tasks/Task_6/README
    	renamed:    Tasks/Task_7/Compiled_1/main.py -> Tasks/Tasks/Task_7/Compiled_1/main.py
    	renamed:    Tasks/Task_7/Compiled_2/main.py -> Tasks/Tasks/Task_7/Compiled_2/main.py
    	renamed:    Tasks/Task_7/Compiled_3/main.py -> Tasks/Tasks/Task_7/Compiled_3/main.py
    	renamed:    Tasks/Task_7/Compiled_4/main.py -> Tasks/Tasks/Task_7/Compiled_4/main.py
    	renamed:    Tasks/Task_7/Compiled_5/main.py -> Tasks/Tasks/Task_7/Compiled_5/main.py
    	renamed:    Tasks/Task_7/Compiled_6/main.py -> Tasks/Tasks/Task_7/Compiled_6/main.py
    	renamed:    Tasks/Task_7/Compiled_7/main.py -> Tasks/Tasks/Task_7/Compiled_7/main.py
    	renamed:    Tasks/Task_7/Compiled_8/main.py -> Tasks/Tasks/Task_7/Compiled_8/main.py
    	renamed:    Tasks/Task_7/PHD CERTIFICATE -> Tasks/Tasks/Task_7/PHD CERTIFICATE
    	new file:   Tasks/Tasks/Task_7/README

---
+++ Tasks/Projects/Project_1/Calculator.py
@@ -0,0 +1,23 @@
+import os
+os.system('clear')
+
+
+# calculator operators
+
+def add(a,b):
+    return a + b
+
+def sub(a,b):
+    return a - b
+
+def mul(a,b):
+    return a / b
+
+def div(a,b):
+    return a * b
+
+
+# main code
+
+print("hello", end=" | ")
+print("welcome to calculator", end=" | ")
