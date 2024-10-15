3b9f90b24ed94d4690920d18d6f494316c5760d3
Arvinder Pal <arvinderpal10@gmail.com>
2024-10-15 22:14:02 +0530

    Recursion in Python

---
+++ Online_Class/Day13/001.Recursion.py
@@ -0,0 +1,23 @@
+# A function shpould be called to perform any operation / task 
+# We can directly call the functions / A function can be called ny another function
+# A function when it is called by itself --- Recursion
+
+
+# def Hello():
+#     print("Hello")
+#     Hello()
+
+# Hello()
+
+
+
+def Decreement(x):
+    if x<0:
+        return 0
+    else:
+        
+        Decreement(x-1)
+        print(x)
+        
+
+Decreement(10)
